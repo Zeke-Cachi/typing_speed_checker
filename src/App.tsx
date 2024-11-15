e763328413be4d546835fbded27fc5f89711e0a2
Zeke-Cachi <ezequielcachi@gmail.com>
2024-11-15 15:28:36 -0300

    made timer - started with results

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { KeyboardEvent, useState } from "react";
+import { KeyboardEvent, useState, useRef, useEffect } from "react";
 
 export default function App() {
+  const paragraphRef = useRef<HTMLDivElement>(null);
+
   const [word] = useState(
     "hello from the gutter. Im a very sophisticated person which likes to fancy around in big cars and do stuff".split(
       ""
@@ -8,8 +10,38 @@ export default function App() {
   );
   const [wordArray, setWordArray] = useState<string[]>([]);
   const [fontColor, setFontColor] = useState<string[]>([]);
+  const [startTyping, setStartTyping] = useState<boolean>(false);
+  const [showResults, setShowResults] = useState<boolean>(false);
+  const [timer, setTimer] = useState<{ seconds: number; miliseconds: number }>({
+    seconds: 0,
+    miliseconds: 0,
+  });
+
+  useEffect(() => {
+    let interval: number;
+    if (startTyping === true) {
+      interval = setInterval(() => {
+        setTimer((prev) => {
+          const newMilliseconds = prev.miliseconds + 1;
+          if (newMilliseconds === 100) {
+            if (prev.seconds === 59) {
+              clearInterval(interval);
+              setStartTyping(false);
+              setShowResults(true);
+              return { ...prev, miliseconds: 0, seconds: 0 };
+            }
+            return { seconds: prev.seconds + 1, miliseconds: 0 };
+          } else {
+            return { ...prev, miliseconds: newMilliseconds };
+          }
+        });
+      }, 10);
+    }
+    return () => clearInterval(interval);
+  }, [startTyping]);
 
   const handleTyping = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (!startTyping) return;
     if (e.key === "Delete" || e.key === "Backspace") {
       setWordArray((prev) => prev.slice(0, -1));
       setFontColor((prev) => prev.slice(0, -1));
@@ -17,6 +49,9 @@ export default function App() {
     } else if (e.key === "Shift") return;
     const temporalWordArr = [...wordArray, e.key];
     setWordArray((oldValue) => [...oldValue, e.key]);
+    if (temporalWordArr.length === word.length) {
+      setStartTyping(false);
+    }
     return temporalWordArr[temporalWordArr.length - 1] ===
       word[temporalWordArr.length - 1]
       ? setFontColor((prev) => [...prev, "text-green-500"])
@@ -28,21 +63,55 @@ export default function App() {
         ]);
   };
 
+  const handleClick = () => {
+    setStartTyping(true);
+    if (paragraphRef.current) {
+      paragraphRef.current.focus();
+    }
+  };
+
+  const formatNumber = (num: number) => num.toString().padStart(2, "0");
+
   return (
     <main className="lg:p-24 mx-auto text-center grid lg:gap-12">
       <h1>Typing Speed Checker</h1>
-      <div
-        onKeyDown={(e) => handleTyping(e)}
-        tabIndex={0}
-        className="focus:outline-none"
+      {!showResults ? (
+        <div
+          onKeyDown={(e) => handleTyping(e)}
+          tabIndex={0}
+          className="focus:outline-none"
+          ref={paragraphRef}
+        >
+          {word.map((element, i) => (
+            <span key={i} className={`font-bold ${fontColor[i]}`}>
+              {element}
+            </span>
+          ))}
+        </div>
+      ) : (
+        <p>
+          You had:{" "}
+          <span>
+            {fontColor.reduce(
+              (acc, element) =>
+                element === "text-red-500" || element === "bg-red-500"
+                  ? acc + 1
+                  : acc,
+              0
+            )}
+          </span>{" "}
+          errors
+        </p>
+      )}
+      <p className="text-2xl">
+        <span>{formatNumber(timer.seconds)}</span>:
+        <span>{formatNumber(timer.miliseconds)}</span>
+      </p>
+      <button
+        className="bg-blue-500 text-white w-20 h-12 rounded-md font-bold text-lg mx-auto hover:bg-blue-400 disabled:bg-gray-300 disabled:text-gray-800"
+        onClick={() => handleClick()}
+        disabled={startTyping}
       >
-        {word.map((element, i) => (
-          <span key={i} className={`font-bold ${fontColor[i]}`}>
-            {element}
-          </span>
-        ))}
-      </div>
-      <button className="bg-blue-500 text-white w-20 h-12 rounded-md font-bold text-lg mx-auto">
         Start
       </button>
     </main>
