0c75c4fdec78962c8e9a4b06d033c79eb9593183
Zeke-Cachi <ezequielcachi@gmail.com>
2024-11-15 11:55:18 -0300

    fixed error with uppercase

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
       setWordArray((prev) => prev.slice(0, -1));
       setFontColor((prev) => prev.slice(0, -1));
       return;
-    }
+    } else if (e.key === "Shift") return;
     const temporalWordArr = [...wordArray, e.key];
     setWordArray((oldValue) => [...oldValue, e.key]);
     return temporalWordArr[temporalWordArr.length - 1] ===
@@ -29,12 +29,22 @@ export default function App() {
   };
 
   return (
-    <div className="App" onKeyDown={(e) => handleTyping(e)} tabIndex={0}>
-      {word.map((element, i) => (
-        <span key={i} className={`font-bold ${fontColor[i]}`}>
-          {element}
-        </span>
-      ))}
-    </div>
+    <main className="lg:p-24 mx-auto text-center grid lg:gap-12">
+      <h1>Typing Speed Checker</h1>
+      <div
+        onKeyDown={(e) => handleTyping(e)}
+        tabIndex={0}
+        className="focus:outline-none"
+      >
+        {word.map((element, i) => (
+          <span key={i} className={`font-bold ${fontColor[i]}`}>
+            {element}
+          </span>
+        ))}
+      </div>
+      <button className="bg-blue-500 text-white w-20 h-12 rounded-md font-bold text-lg mx-auto">
+        Start
+      </button>
+    </main>
   );
 }
