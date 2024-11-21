84dbc11b043183dec222d2083f4f704cb5bf5653
Cachi <ezequielcachi@gmail.com>
2024-11-21 10:52:36 -0300

    added audio to keystrokes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ type Language = "english" | "spanish" | null;
 export default function App() {
   const paragraphRef = useRef<HTMLDivElement>(null);
   const spanishAccentRef = useRef<string>("");
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
   //--------------------------------------------------------------------------------------------------------------------------
 
@@ -63,6 +64,10 @@ export default function App() {
 
   //--------------------------------------------------------------------------------------------------------------------------
   const handleTyping = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+      audioRef.current!.play();
+    }
     if (!startTyping || e.key === "Shift") return;
     if (e.key === "Dead") {
       spanishAccentRef.current = "Dead";
@@ -169,89 +174,99 @@ export default function App() {
 
   //--------------------------------------------------------------------------------------------------------------------------
   return (
-    <main className="lg:p-24 mx-auto text-center grid lg:gap-12 relative">
-      <div
-        className={`absolute inset-0 bg-black opacity-50 place-items-center ${
-          countdown === 4 ? "hidden" : "grid"
-        }`}
-      >
-        <p className="text-5xl text-white ">{countdown}</p>
-      </div>
-      <h1>Typing Speed Checker</h1>
-      {!showResults && language === null && (
-        <div className="grid gap-12">
-          <h2>Choose your language</h2>
-          <div className="mx-auto w-1/2 flex gap-12">
-            <Button title="Español" onClick={() => setLanguage("spanish")}>
-              Español
-            </Button>
+    <main className="lg:p-24 mx-auto text-center flex flex-col justify-between lg:gap-12 relative h-screen">
+      <audio ref={audioRef}>
+        <source src="../public/typewriter_sound.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+      <div className="flex flex-col justify-evenly h-[50%]">
+        <div
+          className={`absolute inset-0 bg-black opacity-50 place-items-center ${
+            countdown === 4 ? "hidden" : "grid"
+          }`}
+        >
+          <p className="text-5xl text-white ">{countdown}</p>
+        </div>
+        <h1>Typing Speed Checker</h1>
+        {!showResults && language === null && (
+          <div className="grid gap-12">
+            <h2>Choose your language</h2>
+            <div className="mx-auto w-1/2 flex gap-12">
+              <Button
+                title="Español"
+                onClick={() => setLanguage("spanish")}
+              ></Button>
+              <Button
+                title="English"
+                className="bg-blue-500 text-white w-20 h-12 rounded-md font-bold text-lg mx-auto hover:bg-blue-400 disabled:bg-gray-300 disabled:text-gray-800"
+                onClick={() => setLanguage("english")}
+              ></Button>
+            </div>
+          </div>
+        )}
+        {!showResults && language && (
+          <div
+            onKeyDown={(e) => handleTyping(e)}
+            tabIndex={0}
+            className="focus:outline-none"
+            ref={paragraphRef}
+          >
+            {word!.length > 0 &&
+              word!.map((element, i) => (
+                <span key={i} className={`font-bold ${fontColor[i]}`}>
+                  {element}
+                </span>
+              ))}
+          </div>
+        )}
+        {showResults && (
+          <div className="grid gap-6">
+            You wrote{" "}
+            <span>
+              {wordArray
+                .join("")
+                .split(" ")
+                .reduce((acc, element) => (element ? acc + 1 : acc), 0)}
+            </span>{" "}
+            words
+            <p>
+              You had:{" "}
+              <span>
+                {fontColor.reduce(
+                  (acc, element) =>
+                    element === "text-red-500" || element === "bg-red-500"
+                      ? acc + 1
+                      : acc,
+                  0
+                )}
+              </span>{" "}
+              errors
+            </p>
             <Button
-              title="English"
-              className="bg-blue-500 text-white w-20 h-12 rounded-md font-bold text-lg mx-auto hover:bg-blue-400 disabled:bg-gray-300 disabled:text-gray-800"
-              onClick={() => setLanguage("english")}
+              title="Volver"
+              onClick={() => {
+                resetTurn();
+              }}
             ></Button>
           </div>
-        </div>
-      )}
-      {!showResults && language && (
-        <div
-          onKeyDown={(e) => handleTyping(e)}
-          tabIndex={0}
-          className="focus:outline-none"
-          ref={paragraphRef}
-        >
-          {word!.length > 0 &&
-            word!.map((element, i) => (
-              <span key={i} className={`font-bold ${fontColor[i]}`}>
-                {element}
-              </span>
-            ))}
-        </div>
-      )}
-      {showResults && (
-        <div className="grid gap-6">
-          You wrote{" "}
-          <span>
-            {wordArray
-              .join("")
-              .split(" ")
-              .reduce((acc, element) => (element ? acc + 1 : acc), 0)}
-          </span>{" "}
-          words
-          <p>
-            You had:{" "}
-            <span>
-              {fontColor.reduce(
-                (acc, element) =>
-                  element === "text-red-500" || element === "bg-red-500"
-                    ? acc + 1
-                    : acc,
-                0
-              )}
-            </span>{" "}
-            errors
-          </p>
-          <Button
-            title="Volver"
-            onClick={() => {
-              resetTurn();
-            }}
-          ></Button>
-        </div>
-      )}
-      {language && !showResults && (
-        <>
-          <p className="text-2xl">
-            <span>{formatNumber(timer.seconds)}</span>:
-            <span>{formatNumber(timer.miliseconds)}</span>
-          </p>
-          <Button
-            title="Start"
-            onClick={() => handleClick()}
-            disabled={startTyping}
-          ></Button>
-        </>
-      )}
+        )}
+        {language && !showResults && (
+          <>
+            <p className="text-2xl">
+              <span>{formatNumber(timer.seconds)}</span>:
+              <span>{formatNumber(timer.miliseconds)}</span>
+            </p>
+            <Button
+              title="Start"
+              onClick={() => handleClick()}
+              disabled={startTyping}
+            ></Button>
+          </>
+        )}
+      </div>
+      <div>
+        Do you like this page? you can donate <span>here</span>
+      </div>
     </main>
   );
 }
